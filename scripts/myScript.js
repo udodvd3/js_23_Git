2b065f8852278ff3ecb9992782e0a7efcb2050a6
Your Name <you@example.com>
2019-09-11 07:09:21 +0300

    first commit

---
+++ scripts/myScript.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+
+let myWrap = $('.wraper');
+$('.myBtn').on('click',function (e) {
+    console.log(e.target);
+    let myUl = "<ul></ul>";
+    let myNum = (Math.random()*1000).toFixed(0);
+    console.log(myNum);
+    $('<li>New Num - '+myNum+'</li>').appendTo(myUl).appendTo(myWrap);
+})
+
+
+})
