aa95c57faba6136268760c6198450f3e3332093b
Your Name <you@example.com>
2019-09-12 11:47:45 +0300

    Petya

--- scripts/myScript.js
+++ scripts/myScript.js
@@ -1,26 +1,1 @@
-$(document).ready(function () {
-
-let myWrap = $('.wraper');
-$('.myBtn').on('click',function (e) {
-    console.log(e.target);
-    let myUl = "<ul></ul>";
-    let myNum = (Math.random()*1000).toFixed(0);
-    alert(myNum);
-    $('<li>New Num - '+myNum+'</li>').appendTo(myUl).appendTo(myWrap);
-})
-
-let mySecCommit = 'MySec Commit There';
-alert(mySecCommit);
-
-let materCommit_v3 = "V3.0 Commit";
-
-let myNewBranchCommit = 'New branch Commit is HERE';
-alert(myNewBranchCommit);
-
-let NewBranchCommit_2 = "My sec commit in New Branch";
-//Вопрос 1-ый конфликт с переменными!!
-
-//2-ой - не было возможности замерджить!!
-
-
-})
+$(document).ready(function () {let myWrap = $('.wraper');$('.myBtn').on('click',function (e) {    console.log(e.target);    let myUl = "<ul></ul>";    let myNum = (Math.random()*1000).toFixed(0);    alert(myNum);    $('<li>New Num - '+myNum+'</li>').appendTo(myUl).appendTo(myWrap);})let mySecCommit = 'MySec Commit There';alert(mySecCommit);let materCommit_v3 = "V3.0 Commit";let myNewBranchCommit = 'New branch Commit is HERE';alert(myNewBranchCommit);let NewBranchCommit_2 = "My sec commit in New Branch";//Вопрос 1-ый конфликт с переменными!!//2-ой - не было возможности замерджить!!const MYNEWCONTANTA = '10000 $';alert(MYNEWCONTANTA);})
